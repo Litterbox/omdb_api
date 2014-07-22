25d36ccdb49e7afc9827b3c0938f36733b1d9349
Tim Licata <tim@timothylicata.com>
2014-07-22 01:09:25 -0700

    remove answers

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 var express = require("express");
-var request = require("request");
 
 var app = express();
 
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
-
-app.get('/search', function (req, res) {
-    var term = req.query.search_term;
-    var url = "http://www.omdbapi.com/?s=" + term;
-
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var data = JSON.parse(body);
-            res.render("results.ejs", {movieList: data.Search});
-        }
-    });
-});
-
 
 app.listen(3000);
